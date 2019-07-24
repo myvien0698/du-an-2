e6ece32fcf1d677e8fd4d55113306dd5d2e5bb2e
myvien0698 <myvienk39ccntt23457@gmail.com>
2019-07-24 08:16:10 +0700

    bai 3
    thiet ke GuestBook

---
+++ bai3/bai3/WebForm1.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="bai3.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Tiêu đề:"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="txttieude" runat="server" Width="540px"></asp:TextBox>
+            <br />
+            <asp:Label ID="Label2" runat="server" Text="Họ tên:"></asp:Label>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="txthoten" runat="server" Width="538px"></asp:TextBox>
+            <br />
+            Email:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="txtemail" runat="server" Width="537px"></asp:TextBox>
+            <br />
+            Nội dung:&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="txtnoidung" runat="server" AutoCompleteType="DisplayName" Height="122px" OnTextChanged="TextBox4_TextChanged" Rows="6" TextMode="MultiLine" Width="538px"></asp:TextBox>
+          <br />
+            <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="btnguiND" runat="server" OnClick="btnguiND_Click" Text="Gửi nội dung" Width="122px" />
+            <br />
+            <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ bai3/bai3/WebForm1.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace bai3
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        private readonly object writer;
+        private readonly int entry;
+        private object StringSpilitOptions;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // hiển thị các entry comment trước
+            ReadComment();
+        }
+
+       
+        protected void btnguiND_Click(object sender, EventArgs e)
+        {
+            // tao file theo duong dan wedsite
+            DateTime date = DateTime.Now;
+            string sfile = Server.MapPath(@"\")
+                + "data.txt";
+            //mở file nếu khác rỗng thì thêm
+            using (StreamWriter writer = new StreamWriter(sfile, true))
+            {
+                //thêm các entry cmt mới
+                writer.WriteLine(txttieude.Text);
+                writer.WriteLine(txthoten.Text);
+                writer.WriteLine(txtemail.Text);
+                writer.WriteLine(txtnoidung.Text);
+                //thêm ký hiệu nhận dạng kết thúc 1 entry để lọc ra từng entry
+                writer.WriteLine("#END");
+                writer.Close();
+            }
+        }
+        private void ReadComment()
+        {
+
+            //hàm đọc các comment trong file và đưa cào bảng trên wed
+            string sfile = Server.MapPath(@"\") + "data.txt";
+            // mở file, nếu khác rỗng thì bắt đầu đọc
+            using (StreamWrite reader = new StreamWrite(sfile: sfile))
+            {
+                // đọc toàn bộ file
+                string snoidung = reader.ReadToEnd();
+                string[] delimiter = { "#END" };
+                // lọc ra từng entry và đưa vào mảng chuổi
+                string[] sArr = snoidung.Split(delimiter, StringSpilitOptions.RemoveEmptyEntries);
+                // duyệt qua từng entry
+                foreach(string s in sArr)
+                {
+                    string stemp;
+                    // thay thế ký tự " xuống dòng và đầu dòng" bằng tab break
+                    stemp = Regex.Replace(s, @"\r\n", @"<\br>");
+                    // tạo ra các dòng hiển thị entry
+                    EntryComment.InnerHtml += entry;
+                }
+            }
+        }
+    }
+}
